c8fe250d0563740967585a0b101ec52a737193f8
hetdesai <hetdesai13@gmail.com>
2021-07-24 11:32:33 +0530

    Add files via upload
    Uploading program.cs file.

---
+++ FileData/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThirdPartyTools;
+using FileData.BL;
+namespace FileData
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ValidateArgument  validateArgument = new ValidateArgument();
+            validateArgument.CheckArgument(args);
+            Console.ReadLine();
+        }
+
+
+    }
+}
